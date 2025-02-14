5e94f1e033a9d0167085afd6209fa9e2032772fe
Marina <marina.ua13@gmail.com>
2025-02-14 11:16:07 +0200

    rewrote class CartPage, correct the mistakes

--- test/pageobjects/CartPage.js
+++ test/pageobjects/CartPage.js
@@ -1,8 +1,13 @@
 class CartPage {
     get checkoutButton() { return $('#checkout'); }
+    get cartItems() { return $$('.cart_item'); }
 
     async proceedToCheckout() {
         await this.checkoutButton.click();
+    }
+
+    async getCartItems() {
+        return this.cartItems;
     }
 }
 

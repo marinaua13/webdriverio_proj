394399a3f2fe13af3d84e60952c86196f911889c
Marina <marina.ua13@gmail.com>
2025-02-14 11:18:49 +0200

    rewrote class InventoryPage, correct the mistakes

--- test/pageobjects/InventoryPage.js
+++ test/pageobjects/InventoryPage.js
@@ -2,6 +2,7 @@ class InventoryPage {
     get addToCartButton() { return $('#add-to-cart-sauce-labs-backpack'); }
     get cartBadge() { return $('.shopping_cart_badge'); }
     get cartButton() { return $('.shopping_cart_link'); }
+    get cartItemPrices() { return $$('.inventory_item_price'); }
 
     async addItemToCart() {
         await this.addToCartButton.click();
@@ -13,6 +14,15 @@ class InventoryPage {
 
     async openCart() {
         await this.cartButton.click();
+    }
+
+    async getTotalAddedItemsPrice() {
+        const prices = await this.cartItemPrices.map(async (priceElement) => {
+            const priceText = await priceElement.getText();
+            return parseFloat(priceText.replace('$', ''));
+        });
+        const pricesResolved = await Promise.all(prices);
+        return `$${pricesResolved.reduce((acc, price) => acc + price, 0).toFixed(2)}`;
     }
 }
 

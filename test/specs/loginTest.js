99533aaa61fb41047327f12f266f9fbe6750e357
Marina <marina.ua13@gmail.com>
2025-02-14 11:23:59 +0200

    rewrote code in loginTest.js

--- test/specs/loginTest.js
+++ test/specs/loginTest.js
@@ -1,21 +1,20 @@
-// import LoginPage from '../pageobjects/LoginPage.js';
-//
-// describe('Login tests', () => {
-//     it('should login with valid credentials', async () => {
-//         await LoginPage.open();
-//         await LoginPage.login('standard_user', 'secret_sauce');
-//         await expect(browser).toHaveUrlContaining('inventory.html');
-//     });
-// });
 import LoginPage from '../pageobjects/LoginPage.js';
+import {USER, BASE_URL} from '../config/constants.js';
 
-describe('Login tests', () => {
-    it('should login with valid credentials', async () => {
+describe('Login Tests', () => {
+    before(async () => {
         await LoginPage.open();
-        await LoginPage.login('standard_user', 'secret_sauce');
+    });
 
-        // Перевірка через альтернативний метод
+    it('should login with valid credentials', async () => {
+        await LoginPage.login(USER.STANDARD.username, USER.STANDARD.password);
         const currentUrl = await browser.getUrl();
         expect(currentUrl).toContain('inventory.html');
     });
+    it('should not login with invalid credentials', async () => {
+        await LoginPage.open();
+        await LoginPage.login('wrong_user', 'wrong_password');
+        const errorMessage = await LoginPage.getErrorMessage();
+        await expect(errorMessage).toContain('Username and password do not match');
+});
 });

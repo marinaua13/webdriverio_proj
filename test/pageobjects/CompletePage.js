d1aaf39dbee20329b30abbd7751c05609bb08a03
Marina <marina.ua13@gmail.com>
2025-02-14 11:17:59 +0200

    rewrote class CompletePage, correct the mistakes

--- test/pageobjects/CompletePage.js
+++ test/pageobjects/CompletePage.js
@@ -1,14 +1,25 @@
 class CompletePage {
-    get backHomeButton() { return $('#back-to-products'); }
-    get thankYouMessage() { return $('.complete-header'); }
+    get backHomeButton() {
+        return $('#back-to-products');
+    }
 
-    async backToHome() {
-        await this.backHomeButton.waitForExist({ timeout: 5000 });
+    get thankYouMessage() {
+        return $('.complete-header');
+    }
+
+    async clickBackHome() {
+        await this.backHomeButton.waitForExist({
+            timeout: 5000,
+            timeoutMsg: 'Button "Back Home" not show up'
+        });
         await this.backHomeButton.click();
     }
 
     async getThankYouMessage() {
-        await this.thankYouMessage.waitForExist({ timeout: 5000 });
+        await this.thankYouMessage.waitForExist({
+            timeout: 5000,
+            timeoutMsg: 'Message "Thank You" not show up'
+        });
         return await this.thankYouMessage.getText();
     }
 }

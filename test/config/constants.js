6df425073df0e7daf1d1cef4e67c0aa211cded59
Marina <marina.ua13@gmail.com>
2025-02-14 11:22:11 +0200

    create files constants.js and paths.js in a folder config

---
+++ test/config/constants.js
@@ -0,0 +1,14 @@
+export const USER = {
+    STANDARD: {
+        username: 'standard_user',
+        password: 'secret_sauce',
+    },
+};
+
+export const CHECKOUT_DATA = {
+    firstName: 'John',
+    lastName: 'Doe',
+    postalCode: '12345',
+};
+
+export const BASE_URL = 'https://www.saucedemo.com';
---
+++ test/config/paths.js
@@ -0,0 +1,5 @@
+export const PATHS = {
+    LOGIN: '/',
+    CHECKOUT_STEP_ONE: '/checkout-step-one.html',
+    CHECKOUT_STEP_TWO: '/checkout-step-two.html',
+};
